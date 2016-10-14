738c520f443dc177faf45c857a3be0145b80035f
Kei Yasuda <kei_yasuda@outlook.com>
2016-10-15 03:06:07 +0900

    implementing FixedPool

--- src/memory.c
+++ src/memory.c
@@ -13,6 +13,8 @@ typedef struct MemoryPool
 struct MemoryPoolMethodTable
 {
 	void	(*delete)(MemoryPool* self);
+	void*	(*allocate)(MemoryPool* self);
+	void	(*free)(MemoryPool* self, void* ptr);
 };
 
 // ------------------------------------------------------------------
@@ -36,11 +38,37 @@ void MemoryPool_delete(
 }
 
 // ------------------------------------------------------------------
+void* MemoryPool_allocate(
+	MemoryPool* self
+	)
+{
+	if(self->methodTable->allocate != NULL)
+	{
+		self->methodTable->allocate(self);
+	}
+}
+
+// ------------------------------------------------------------------
+void MemoryPool_free(
+	MemoryPool* self,
+	void* ptr
+	)
+{
+	if(self->methodTable->free != NULL)
+	{
+		self->methodTable->free(self);
+	}
+}
+
+// ------------------------------------------------------------------
 // FixedPool
 // ------------------------------------------------------------------
 typedef struct FixedPool
 {
 	MemoryPool interface;
+	void* memory;
+	void** used;
+	void** free;
 }FixedPool;
 
 // ------------------------------------------------------------------
@@ -48,16 +76,26 @@ static const MemoryPoolMethodTable gFixedPoolMethodTable;
 
 // ------------------------------------------------------------------
 static bool __FixedPool_initialize(
-	FixedPool* self
+	FixedPool* self,
+	size_t sizeofMemory,
+	size_t numofMemory
 	)
 {
 	MemoryPool_setMethodTable((MemoryPool*)self, &gFixedPoolMethodTable);
+
+	self->memory = Malloc(sizeofMemory * numofMemory);
+	if(self->memory == NULL)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 // ------------------------------------------------------------------
 FixedPool* FixedPool_create(
-	void
+	size_t sizeofMemory,
+	size_t numofMemory
 	)
 {
 	FixedPool* self;
@@ -95,8 +133,29 @@ static void __FixedPool_delete(
 }
 
 // ------------------------------------------------------------------
+static void* __FixedPool_allocate(
+	MemoryPool* interface
+	)
+{
+	FixedPool* self = (FixedPool*)interface;
+
+}
+
+// ------------------------------------------------------------------
+static void __FixedPool_free(
+	MemoryPool* interface,
+	void* ptr
+	)
+{
+	FixedPool* self = (FixedPool*)interface;
+
+}
+
+// ------------------------------------------------------------------
 static const MemoryPoolMethodTable gFixedPoolMethodTable =
 {
-	.delete = __FixedPool_delete,
+	.delete		= __FixedPool_delete,
+	.allocate	= __FixedPool_allocate,
+	.free		= __FixedPool_free,
 };
 
